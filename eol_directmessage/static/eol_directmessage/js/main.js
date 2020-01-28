281a987b37b314de4c06bd53be6606a6c8eaa0b3
matsalinas <matias.salinas.s@mail.pucv.cl>
2020-01-28 15:37:39 +0000

    New Chat

--- eol_directmessage/static/eol_directmessage/js/main.js
+++ eol_directmessage/static/eol_directmessage/js/main.js
@@ -2,7 +2,47 @@ $( document ).ready(function() {
     $("#dmChats").hide();
     $(".send-message").hide();
     get_chats();
+
+    /*
+    * Add student to the chat list
+    * Open an empty chat
+    */
+    $("#all_students_list li a").click(function(e) {
+        // Get chat attributes
+        let a_class = $(this).attr('class');
+        let other_username = a_class.replace('list-','');
+        let other_name = $(this).text();
+        let chat = {
+            'sender_user__username' : USER_USERNAME,
+            'receiver_user__username' : other_username,
+            'receiver_user__profile__name' : other_name,
+            'min_viewed' : true
+        };
+        
+        // Remove 'no chats' message when student doesn't have chats
+        if ($('#list .recent-messages-list').text() == 'No tienes conversaciones recientes.') {
+            $('#list .recent-messages-list').html('');
+        }
+
+        // Add user to the chat list
+        generate_list_html(chat);
+
+        // Remove from all students list
+        $(this).remove(); 
+
+        // Update click handler
+        $('.user-list').click(function() {
+            other_username = $(this).attr('id');
+            get_messages(other_username);
+        });
+
+        // Open new empty chat
+        get_messages(other_username); 
+    });
     
+    /*
+    * Submit a new message
+    */
     $('#new-message-form').submit(function(e) {
                 
         var url = URL_NEW_MESSAGE;
@@ -25,7 +65,7 @@ $( document ).ready(function() {
             },
             success:  function (response) {
                 /*
-                * Set input status: correct
+                * Reset input value, and reload chat messages
                 */
                 $('#new-message').val('');
                 get_messages(params.other_username);
@@ -46,25 +86,36 @@ $( document ).ready(function() {
         e.preventDefault();
     });
 
+    /*
+    * Get chat lists with info from another users
+    */
     function get_chats(){
         var url = URL_GET_STUDENT_CHAT;
         $.ajax({
             url:   url,
             beforeSend: function () {
+                // Show loading
                 $('#list-loading').show();
             },
             success:  function (response) {
                 user_data = response;
-                console.log(user_data)
+
+                // Reset html div
                 $('#list .recent-messages-list').html('');
+
+                // Show message if user doesnt' have chats already
+                if (user_data.length == 0)  $('#list .recent-messages-list').html('No tienes conversaciones recientes.');
+
+                // Create list for each user
                 for(chat of user_data) {
                     generate_list_html(chat);
                 }
+                // Click handler. Show users messages on click.
                 $('.user-list').click(function() {
                     other_username = $(this).attr('id');
                     get_messages(other_username);
                 });
-                // set keyup function after all student list is loaded and uptaded  
+                // Set keyup function after all student list is loaded and uptaded  
                 $('#search_input').keyup(search);
             },
             error: function (xhr, ajaxOptions, thrownError) {
@@ -74,11 +125,16 @@ $( document ).ready(function() {
                 console.log(thrownError);
             },
             complete: function() {
+                // Hide loading
                 $('#list-loading').hide();
             }
         });
     }
 
+
+    /*
+    * Get all messages between two users (logged user and another)
+    */
     function get_messages(other_username) {
         $('#username-message').val(other_username);
         var url = URL_GET_MESSAGES;
@@ -86,17 +142,28 @@ $( document ).ready(function() {
         $.ajax({
             url:   url,
             beforeSend: function () {
+                // Show loading and scroll to the chat
                 $('#messages-loading').show();
+                $('html,body').animate({
+                    scrollTop: $("#messages-loading").offset().top
+                }, 'slow');
             },
             success:  function (response) {
-                console.log(response);
                 data = response;
+
+                // Reset html div
                 $('#dmChats').html('');
+
+                // Show each message
                 for(message of data) {
                     generate_messages_html(message);
                 }
                 $("#dmChats").show();
+
+                // Scroll to bottom (end of messages)
                 $("#dmChats").scrollTop($("#dmChats").prop("scrollHeight"));
+
+                // Show submit input/button
                 $(".send-message").show();
             },
             error: function (xhr, ajaxOptions, thrownError) {
@@ -106,26 +173,32 @@ $( document ).ready(function() {
                 console.log(thrownError);
             },
             complete: function() {
+                // Hide loading
                 $('#messages-loading').hide();
             }
         });
     }
 
+    /*
+    * Generate element on list from each user
+    */
     function generate_list_html(chat) {
         username = USER_USERNAME;
-        //#list .recent-messages-list
-        if (username == chat.sender_user__profile__name) {
+        // Define otherusername 
+        if (username == chat.sender_user__username) {
             other_username = chat.receiver_user__username;
             profile_name = chat.receiver_user__profile__name;
         } else {
             other_username = chat.sender_user__username; 
             profile_name = chat.sender_user__profile__name;
         }
-        if (chat.min_viewed || username == chat.sender_user__profile__name)
+        // min_viewed boolean: false if the user didn't read all messages
+        if (chat.min_viewed || username == chat.sender_user__username)
             status = "";
         else
             status = "<strong>Tienes nuevos mensajes !</strong>";
 
+        // Append <li> element with other user info
         $('#list .recent-messages-list').append(
             '<li class="user-list" id="' + other_username + '">' + 
                 '<span class="icon-list"><i class="fa fa-chevron-right"></i></span>'+
@@ -141,10 +214,15 @@ $( document ).ready(function() {
         $('.list-' + other_username).remove();
     }
 
+    /*
+    * Generate each message from chat
+    */
     function generate_messages_html(message) {
         username = USER_USERNAME;
         date = new Date(message.created_at.$date);
+        // Define sender and receiver user and set respective class
         if ( message.receiver_user__username == username ) {
+            // Escape html in message.text
             let dm = '<div class="otherDM other chat-message">' + escapeHtml(message.text) + '</div>';
             let detail = '<span class="other">' + message.sender_user__profile__name + ' - ' + date.toLocaleString() + '</span>';
             var dmWrapper = '<div class="dmWrapper">' +
@@ -164,9 +242,13 @@ $( document ).ready(function() {
                             '</div>';
         }
         
+        // Append div to the direct messages chat
         $('#dmChats').append(dmWrapper);
     };
 
+    /*
+    * Search function used in all student lists
+    */
     function search() {
         // Declare variables
         var input, filter, ul, li, a, i, txtValue;
@@ -189,6 +271,9 @@ $( document ).ready(function() {
         }
     }
 
+    /*
+    * Escape HTML from string
+    */
     var entityMap = {
         '&': '&amp;',
         '<': '&lt;',
@@ -198,8 +283,7 @@ $( document ).ready(function() {
         '/': '&#x2F;',
         '`': '&#x60;',
         '=': '&#x3D;'
-      };
-      
+    };
     function escapeHtml (string) {
         return String(string).replace(/[&<>"'`=\/]/g, function (s) {
           return entityMap[s];
